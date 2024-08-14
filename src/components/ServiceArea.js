1e5ce3370e659e54e43ba9fb93cd4eccebf799e6
Jakub Stadnik <jakub.stadnik@vitave.com>
2024-08-14 15:08:54 +0200

    s

--- src/components/ServiceArea.js
+++ src/components/ServiceArea.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import servicesData from '../data/services.json';
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronRight,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
+import servicesData from "../data/services.json";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-const serviceIcons = ["/assets/icons/icon1.svg", "/assets/icons/icon2.svg", "/assets/icons/icon3.svg", "/assets/icons/icon4.svg"];
+const serviceIcons = [
+  "/assets/icons/icon1.svg",
+  "/assets/icons/icon2.svg",
+  "/assets/icons/icon3.svg",
+  "/assets/icons/icon4.svg",
+];
 
 const fadeInUpVariants = {
   hidden: { opacity: 0, y: 20 },
@@ -13,7 +21,7 @@ const fadeInUpVariants = {
 };
 
 const ServiceArea = () => {
-  const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.2 });
 
   return (
     <div className="service-area-start rts-service-area rts-section-gap bg-inner-light">
@@ -24,52 +32,78 @@ const ServiceArea = () => {
               className="service-full-top-wrapper rts-slide-up"
               ref={ref}
               initial="hidden"
-              animate={inView ? 'visible' : 'hidden'}
+              animate={inView ? "visible" : "hidden"}
               variants={fadeInUpVariants}
               transition={{ duration: 0.5 }}
             >
               <div className="title-style-left">
                 <div className="pre-title-area">
-                  <img src="assets/images/about/02.png" alt="about" />
+                  <img
+                    src="assets/images/about/02.png"
+                    alt="about"
+                    width="50"
+                    height="50"
+                  />
                   <span className="pre-title">Naše služby</span>
                 </div>
                 <h2 className="title">Poskytované služby</h2>
               </div>
               <p className="disc">
-                V Lenka Design se specializujeme na vytváření mimořádných obytných prostor, které vyzařují eleganci.
+                V Lenka Design se specializujeme na vytváření mimořádných
+                obytných prostor, které vyzařují eleganci.
               </p>
-              <a href="/nase-sluzby" className="rts-read-more-circle-btn under-line">
-                <i> <FontAwesomeIcon icon={faArrowRight} /></i>
+              <a
+                href="/nase-sluzby"
+                className="rts-read-more-circle-btn under-line"
+              >
+                <i>
+                  {" "}
+                  <FontAwesomeIcon icon={faArrowRight} />
+                </i>
                 <p>Zobrazit všechny služby</p>
               </a>
             </motion.div>
           </div>
         </div>
 
-        <div className="row mt--20 g-5 rts-slide-up" style={{ justifyContent: "center" }}>
+        <div
+          className="row mt--20 g-5 rts-slide-up"
+          style={{ justifyContent: "center" }}
+        >
           {servicesData.slice(0, 4).map((service, index) => (
             <motion.div
               className="col-lg-3 col-md-6 col-sm-6 col-12"
               key={index}
               ref={ref}
               initial="hidden"
-              animate={inView ? 'visible' : 'hidden'}
+              animate={inView ? "visible" : "hidden"}
               variants={fadeInUpVariants}
               transition={{ duration: 0.5, delay: index * 0.1 }}
             >
-              <div className="single-service-style-five" style={{ width: "300px" }}>
+              <div
+                className="single-service-style-five"
+                style={{ width: "300px" }}
+              >
                 <div className="icon-area">
-                  <img src={serviceIcons[index]} alt="Architectural Design" />
+                  <img
+                    src={serviceIcons[index]}
+                    alt="Architectural Design"
+                    width="50"
+                    height="50"
+                    loading="lazy"
+                  />
                 </div>
                 <div className="body">
-                  <a href={`/nase-sluzby/${service.nav}`} >
+                  <a href={`/nase-sluzby/${service.nav}`}>
                     <h6 className="title">{service.title}</h6>
                   </a>
-                  <p className="disc">
-                    {service.details[0]}
-                  </p>
-                  <a href={`/nase-sluzby/${service.nav}`} className="read-more-btn">
-                    Přečtěte si více<FontAwesomeIcon icon={faChevronRight} />
+                  <p className="disc">{service.details[0]}</p>
+                  <a
+                    href={`/nase-sluzby/${service.nav}`}
+                    className="read-more-btn"
+                  >
+                    Přečtěte si více
+                    <FontAwesomeIcon icon={faChevronRight} />
                   </a>
                 </div>
               </div>
